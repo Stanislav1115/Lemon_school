4fdfe6bba893a690d7b51013cc416f541fcfb4b3
Stanislav <stanislav.grishchenko1998@gmail.com>
2022-12-28 21:52:40 +0300

    1_Programm_1

--- StartClass.java
+++ StartClass.java
@@ -5,20 +5,13 @@ public class StartClass {
         {
             System.out.println("Please, enter numbers");
             Scanner in = new Scanner(System.in);
-            int numb = in.nextInt();
-            int reSult = StartClass.sum(numb);
+            int firstNumb = in.nextInt();
+            int secondNumb = in.nextInt();
+            int reSult = StartClass.sum(firstNumb, secondNumb);
             System.out.println("result: " + reSult);
         }
-        public static Integer sum(int numb)
+        public static Integer sum(int firstNumb, int secondNumb)
         {
-              int result = 0;
-              for(int i = 0; i <= numb; i++)
-              {
-                  if(i % 3 == 0 || i % 5 == 0)
-                  {
-                      result += i;
-                  }
-              }
-              return result;
+              return firstNumb + secondNumb;
         }
 }
