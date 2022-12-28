b174fa1358cb5dae3e3588c1efea5e7d1c91671c
Stanislav1115 <62393265+Stanislav1115@users.noreply.github.com>
2022-12-29 00:08:39 +0300

    Update StartClass.java

--- StartClass.java
+++ StartClass.java
@@ -3,15 +3,33 @@ import java.util.Scanner;
 public class StartClass {
         public static void main(String[] args)
         {
-            System.out.println("Please, enter numbers");
-            Scanner in = new Scanner(System.in);
-            int firstNumb = in.nextInt();
-            int secondNumb = in.nextInt();
-            int reSult = StartClass.sum(firstNumb, secondNumb);
-            System.out.println("result: " + reSult);
+            System.out.print("First integer: ");
+            int firstNumb = check_value();
+            System.out.print("Second integer: ");
+            int secondNumb = check_value();
+            int result = StartClass.sum(firstNumb, secondNumb);
+            System.out.println("result: " + result);
         }
         public static Integer sum(int firstNumb, int secondNumb)
-        {
-              return firstNumb + secondNumb;
+         {
+            return firstNumb + secondNumb;
+         }
+
+        public static int check_value(){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter an integer: ");
+            int range;
+            while(true){
+                if(input.hasNextInt()){
+                    range = input.nextInt();
+                    if(0>=range || range > 0)
+                        break;
+                    else
+                        continue;
+                }
+                input.nextLine();  //Comsume the garbage value
+                System.out.println("Enter an integer: ");
+            }
+            return range;
         }
 }
