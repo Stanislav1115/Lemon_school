a699093cee7a09f200d26e86b88c908fdd4f6dbe
Stanislav1115 <62393265+Stanislav1115@users.noreply.github.com>
2022-12-29 00:22:37 +0300

    Update StartClass.java

--- StartClass.java
+++ StartClass.java
@@ -3,18 +3,39 @@ import java.util.Scanner;
 public class StartClass {
         public static void main(String[] args)
         {
-            System.out.println("Please, enter numbers");
-            Scanner in = new Scanner(System.in);
-            int numb = in.nextInt();
-            int result = StartClass.faktorial(numb);
+            int numb = check_value();
+            int result = StartClass.sum(numb);
             System.out.println("result: " + result);
         }
-        public static Integer faktorial(int numb)
+
+
+
+    public static int check_value(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int range;
+        while(true){
+            if(input.hasNextInt()){
+                range = input.nextInt();
+                if(0>=range || range > 0)
+                    break;
+                else
+                    continue;
+            }
+            input.nextLine();  //Comsume the garbage value
+            System.out.println("Enter an integer: ");
+        }
+        return range;
+    }
+        public static Integer sum(int numb)
         {
-              int result = 1;
-              for(int i = numb; i > 0; i--)
+              int result = 0;
+              for(int i = 0; i <= numb; i++)
               {
-                      result *= i;
+                      if(i%3 == 0 || i%5 == 0)
+                      {
+                          result += i;
+                      }
               }
               return result;
         }
