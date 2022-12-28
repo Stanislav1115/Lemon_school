4cb6759377195df792184b3f184371003a2a77f2
Stanislav <stanislav.grishchenko1998@gmail.com>
2022-12-28 23:48:57 +0300

    3_1Programm

--- StartClass.java
+++ StartClass.java
@@ -3,12 +3,28 @@ import java.util.Scanner;
 public class StartClass {
         public static void main(String[] args)
         {
-            System.out.println("Please, enter numbers");
-            Scanner in = new Scanner(System.in);
-            int numb = in.nextInt();
+            int numb = check_value();
             int result = StartClass.faktorial(numb);
             System.out.println("result: " + result);
         }
+
+    public static int check_value(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int range;
+        while(true){
+            if(input.hasNextInt()){
+                range = input.nextInt();
+                if(0>=range || range >= 0)
+                    break;
+                else
+                    continue;
+            }
+            input.nextLine();  //Comsume the garbage value
+            System.out.println("Enter an integer: ");
+        }
+        return range;
+    }
         public static Integer faktorial(int numb)
         {
               int result = 1;
