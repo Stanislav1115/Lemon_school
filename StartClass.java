ad23f7de15b08d7a6285bdacfc4c9a76afca1ba8
Stanislav <stanislav.grishchenko1998@gmail.com>
2023-01-08 00:26:33 +0200

    1Programm

--- StartClass.java
+++ StartClass.java
@@ -3,34 +3,67 @@ import java.util.Scanner;
 public class StartClass {
         public static void main(String[] args)
         {
-            System.out.println("How many hours are there now?");
+            System.out.println("How many values do you want to check?");
             int numb = check_value();
-            System.out.println(StartClass.mo_Day_Ev_Night(numb));
+            if(numb < 0){
+                numb *= -1;
+            }else if(numb == 0){
+                System.out.println("Goodby!");
+                System.exit(0);
+            }
+            int[] hours = new int [numb];
+            String[] result = new String[numb];
+            for(int i = 0; i < hours.length; i++)
+            {
+                hours[i] = check_hours();
+                result[i] = mo_Day_Ev_Night(hours[i]);
+            }
+            for(int i = 0; i < hours.length; i++)
+            {
+                System.out.println(hours[i] + " = " + result[i]);
+            }
         }
 
-
-
-    public static int check_value(){
+    public static int check_hours(){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter an your hours: ");
         int range;
         while(true){
             if(input.hasNextInt()){
                 range = input.nextInt();
-                if(0<=range || range <= 24)
+                if(0<=range && range <= 24)
                     break;
                 else
+                    System.out.println("Exeption!!! Only 24 hours in a day!");
+                System.out.println("Enter an your hours: ");
                     continue;
             }
             input.nextLine();  //Comsume the garbage value
-            System.out.println("Exeption!!! Only 24 hours in a day!");
-            System.out.println("Enter an your hours: ");
+        }
+        return range;
+    }
+
+    public static int check_value(){
+        Scanner input = new Scanner(System.in);
+        //System.out.print("Enter an your hours: ");
+        int range;
+        while(true){
+            if(input.hasNextInt()){
+                range = input.nextInt();
+                if(0<=range || range < 0)
+                    break;
+                else
+                    continue;
+            }  //Comsume the garbage value
+            System.out.println("Exeption!!! This is not a number");
+            System.out.println("Enter number: ");
+            input.nextLine();
         }
         return range;
     }
         public static String mo_Day_Ev_Night(int numb)
         {
-            if(numb >= 0 && numb <= 3)
+            if((numb >= 0 && numb <= 3) || numb == 24)
             {
                 return "Night";
             } else if (numb >= 4 && numb <= 11)
