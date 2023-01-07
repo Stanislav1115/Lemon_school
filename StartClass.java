bf89bfe0ebc81bb3e0a0dd3863b723fa4ca0b317
Stanislav <stanislav.grishchenko1998@gmail.com>
2023-01-07 17:54:31 +0200

    1Programm

--- StartClass.java
+++ StartClass.java
@@ -3,32 +3,45 @@ import java.util.Scanner;
 public class StartClass {
         public static void main(String[] args)
         {
+            System.out.println("How many hours are there now?");
             int numb = check_value();
-            boolean result = StartClass.isEven(numb);
-            System.out.println("result: " + result);
+            System.out.println(StartClass.mo_Day_Ev_Night(numb));
         }
 
 
 
     public static int check_value(){
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
+        System.out.print("Enter an your hours: ");
         int range;
         while(true){
             if(input.hasNextInt()){
                 range = input.nextInt();
-                if(0>=range || range > 0)
+                if(0<=range || range <= 24)
                     break;
                 else
                     continue;
             }
             input.nextLine();  //Comsume the garbage value
-            System.out.println("Enter an integer: ");
+            System.out.println("Exeption!!! Only 24 hours in a day!");
+            System.out.println("Enter an your hours: ");
         }
         return range;
     }
-        public static Boolean isEven(int numb)
+        public static String mo_Day_Ev_Night(int numb)
         {
-            return (numb%2)==0? true: false;
+            if(numb >= 0 && numb <= 3)
+            {
+                return "Night";
+            } else if (numb >= 4 && numb <= 11)
+            {
+                return "Morning";
+            }else if (numb >= 12 && numb <= 16)
+            {
+                return "Day";
+            }else
+            {
+                return "Evening";
+            }
         }
 }
