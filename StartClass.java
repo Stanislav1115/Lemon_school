ae313cfceffb414158c5c1c36b94d1c129ac1808
Stanislav <stanislav.grishchenko1998@gmail.com>
2023-01-21 15:52:33 +0200

    weekendOrWorkDay

--- StartClass.java
+++ StartClass.java
@@ -3,79 +3,35 @@ import java.util.Scanner;
 public class StartClass {
         public static void main(String[] args)
         {
-            System.out.println("How many values do you want to check?");
-            int numb = check_value();
-            if(numb < 0){
-                numb *= -1;
-            }else if(numb == 0){
-                System.out.println("Goodby!");
-                System.exit(0);
-            }
-            int[] hours = new int [numb];
-            String[] result = new String[numb];
-            for(int i = 0; i < hours.length; i++)
-            {
-                hours[i] = check_hours();
-                result[i] = mo_Day_Ev_Night(hours[i]);
-            }
-            for(int i = 0; i < hours.length; i++)
-            {
-                System.out.println(hours[i] + " = " + result[i]);
-            }
+            System.out.println("What day you want to check? ");
+            Scanner input = new Scanner(System.in);
+            String day = input.nextLine().toUpperCase();
+            System.out.println(day);
+            System.out.println(weekendOrWorkDay(day));
         }
-
-    public static int check_hours(){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter an your hours: ");
-        int range;
-        while(true){
-            if(input.hasNextInt()){
-                range = input.nextInt();
-                if(0<=range && range <= 24)
+    public static String weekendOrWorkDay(String day)
+    {
+        String[] days = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY",
+                "FRIDAY", "SATURDAY" };
+        String ret = null;
+        for(int i = 0; i < days.length; i++)
+        {
+            if(day.equals(days[i]))
+            {
+                if(i >=0 && i <=4)
+                {
+                    ret = "Workday";
                     break;
+                }
                 else {
-                    System.out.println("Exeption!!! Only 24 hours in a day!");
-                    System.out.println("Enter an your hours: ");
-                    continue;
+                    ret = "Weekend";
+                    break;
                 }
             }
-            input.nextLine();  //Comsume the garbage value
-        }
-        return range;
-    }
-
-    public static int check_value(){
-        Scanner input = new Scanner(System.in);
-        //System.out.print("Enter an your hours: ");
-        int range;
-        while(true){
-            if(input.hasNextInt()){
-                range = input.nextInt();
-                if(0<=range || range < 0)
-                    break;
-                else
-                    continue;
-            }  //Comsume the garbage value
-            System.out.println("Exeption!!! This is not a number");
-            System.out.println("Enter number: ");
-            input.nextLine();
-        }
-        return range;
-    }
-        public static String mo_Day_Ev_Night(int numb)
-        {
-            if((numb >= 0 && numb <= 3) || numb == 24)
-            {
-                return "Night";
-            } else if (numb >= 4 && numb <= 11)
-            {
-                return "Morning";
-            }else if (numb >= 12 && numb <= 16)
-            {
-                return "Day";
-            }else
-            {
-                return "Evening";
+            else {
+                ret = "Your data is incorrect";
             }
         }
+        return ret;
+    }
 }
