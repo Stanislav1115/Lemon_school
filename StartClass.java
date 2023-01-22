4479e79d1ac4ddabd5d552a146105d3938a4a7f4
Stanislav <stanislav.grishchenko1998@gmail.com>
2023-01-22 20:55:56 +0200

    WorkWithMap

--- StartClass.java
+++ StartClass.java
@@ -1,37 +1,95 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class StartClass {
-        public static void main(String[] args)
-        {
-            System.out.println("What day you want to check? ");
-            Scanner input = new Scanner(System.in);
-            String day = input.nextLine().toUpperCase();
-            System.out.println(day);
-            System.out.println(weekendOrWorkDay(day));
-        }
-    public static String weekendOrWorkDay(String day)
+    public static void main(String[] args)
     {
-        String[] days = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY",
-                "FRIDAY", "SATURDAY" };
-        String ret = null;
-        for(int i = 0; i < days.length; i++)
+        System.out.println("How many words you want to check? ");
+        int numb = checkValue();
+        if(numb < 0){
+            numb *= -1;
+        }else if(numb == 0){
+            System.out.println("Goodbye!");
+            System.exit(0);
+        }
+        String[] words = new String[numb];
+        for(int i = 0; i < words.length; i++)
         {
-            if(day.equals(days[i]))
-            {
-                if(i >=0 && i <=4)
+            Scanner input = new Scanner(System.in);
+            words[i] = input.nextLine();
+        }
+        numberOfIdenticalWords(createMap(words));
+        getFirst(words);
+        getLast(words);
+    }
+
+    public static Map<String, Integer> createMap(String[] words)
+    {
+        Map<String, Integer> data = new HashMap<String, Integer>();
+        for(int i = 0; i < words.length; i++)
+        {
+            if(!(data.containsKey(words[i]))){
+                data.put(words[i], 0);
+                for(int j = i+1; j < words.length; j++)
                 {
-                    ret = "Workday";
-                    break;
+                    if(words[j].equals(words[i]))
+                    {
+                        data.put(words[i], data.get(words[i]) + 1);
+                    }
                 }
-                else {
-                    ret = "Weekend";
-                    break;
-                }
-            }
-            else {
-                ret = "Your data is incorrect";
             }
         }
-        return ret;
+        return data;
+    }
+    public static void numberOfIdenticalWords(Map<String, Integer> data)
+    {
+        int result = 0;
+        for(Map.Entry<String, Integer> item : data.entrySet()){
+            result += item.getValue();
+        }
+        System.out.println("Number of identical words: " + result);
+        theLargestNumberOfIdenticalWords(data);
+    }
+
+    public static void theLargestNumberOfIdenticalWords(Map<String, Integer> data)
+    {
+        int result = 0;
+        for(Map.Entry<String, Integer> item : data.entrySet()){
+            if(item.getValue() > result)
+            {
+                result = item.getValue();
+            }
+        }
+        for(Map.Entry<String, Integer> item : data.entrySet()){
+            if(item.getValue() == result)
+            {
+                System.out.println("The Largest: " + item.getKey());
+            }
+        }
+    }
+    public static void getFirst(String[] words)
+    {
+        System.out.println(words[0]);
+    }
+    public static void getLast(String[] words)
+    {
+        System.out.println(words[words.length - 1]);
+    }
+    public static int checkValue(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int range;
+        while(true){
+            if(input.hasNextInt()){
+                range = input.nextInt();
+                if(0>=range || range > 0)
+                    break;
+                else
+                    continue;
+            }
+            input.nextLine();  //Comsume the garbage value
+            System.out.println("Enter an integer: ");
+        }
+        return range;
     }
 }
