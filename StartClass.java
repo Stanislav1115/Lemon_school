42fadead05907e4439d44257f6e3e1150777f635
Stanislav <stanislav.grishchenko1998@gmail.com>
2023-01-26 15:25:47 +0200

    NumbersWithDays

--- StartClass.java
+++ StartClass.java
@@ -1,95 +1,183 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class StartClass {
     public static void main(String[] args)
     {
+        System.out.println("How many elements will be in the array: ");
+        int numb = checkValue();
+        int[] array = new int[numb];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = checkValue();
+        }
+        String arrayString = Arrays.toString(array);
+        System.out.println(arrayString);
         Boolean k = true;
         while(k)
         {
-            System.out.println("_________________________________________________");
-            System.out.println("1 - numbers ");
-            System.out.println("2 - days ");
+            System.out.println("!!!--------------------Start MENU--------------------!!!");
+            System.out.println("1  - Multiplicity Cycles ");
+            System.out.println("__________");
+            System.out.println("2  - Non-pairing Cycles ");
+            System.out.println("__________");
+            System.out.println("3 - indices Standart ");
+            System.out.println("__________");
+            System.out.println("4  - sort Cycles ");
+            System.out.println("44 - sort Standart ");
+            System.out.println("__________");
+            System.out.println("5  - numberOfParied Cycles ");
+            System.out.println("__________");
+            System.out.println("6  - numberOfNonParied Cycles ");
+            System.out.println("__________");
+            System.out.println("7  - numberOfMultiplicity Cycles ");
+            System.out.println("__________");
+            System.out.println("8  - sumEverySecond Cycles ");
             System.out.println("Another input - exit ");
+            System.out.println("_________________________________________________");
             System.out.println("What task you want to check?: ");
             System.out.println("");
             System.out.println("");
             int num = checkValue();
             switch(num)
             {
-                case 1: whatNumber();
+                case 1: multiplicityCycles(array);
                     break;
-                case 2: whatDay();
+                case 2: nonPairingCycles(array);
+                    break;
+                case 3: indicesStandart(array);
+                    break;
+                case 4: sortCycles(array);
+                    break;
+                case 44: sortStandart(array);
+                    break;
+                case 5: numberOfPariedCycles(array);
+                    break;
+                case 6: numberOfNonPariedCycles(array);
+                    break;
+                case 7: numberOfMultiplicityCycles(array);
+                    break;
+                case 8: sumEverySecondCycles(array);
                     break;
                 default: System.exit(0);
             }
         }
     }
-    public static void whatNumber()
+    public static void multiplicityCycles(int[] array)
     {
-        System.out.println("_________________________________________________");
-        System.out.println("Start number program..");
-        System.out.println("");
-        int number = checkValue();
-        if(0 <= number && number < 10)
+        System.out.println("Enter the number by which you want the elements of the array to be reduced: ");
+        int num = checkValue();
+        for(int i = 0; i < array.length; i++)
         {
-            System.out.println("One digit in number");
-        }else if(10 <= number && number < 100)
-        {
-            System.out.println("Two digits in number");
-        }
-        else if(100 <= number && number < 1000)
-        {
-            System.out.println("Three digits in number");
-        }
-        else if(1000 <= number && number < 10000)
-        {
-            System.out.println("Four digits in number");
-        }
-        else if(10000 <= number && number < 100000)
-        {
-            System.out.println("Five digits in number");
-        }
-        else
-        {
-            System.out.println("Not available type");
+            if((array[i] >= num) && (array[i] % num == 0))
+            {
+                System.out.println(array[i]);
+            }
         }
     }
 
-    public static void whatDay()
+    public static void nonPairingCycles(int[] array)
     {
-        System.out.println("_________________________________________________");
-        System.out.println("Start day program..");
-        System.out.println("");
-        int counter = 1;
-        while (counter<3)
+        int sum = 0;
+        for(int i = 0; i < array.length; i++)
         {
-            int num = checkValue();
-            counter++;
-            switch(num)
+            if(array[i] % 2 != 0 )
             {
-                case 1:
-                    System.out.println("Sunday");
-                    break;
-                case 2:
-                    System.out.println("Monday");
-                    break;
-                case 3:
-                    System.out.println("Tuesday");
-                    break;
-                case 4:
-                    System.out.println("Wednesday");
-                    break;
-                case 5:
-                    System.out.println("Thursday");
-                    break;
-                case 6:
-                    System.out.println("Friday");
-                    break;
-                case 7:
-                    System.out.println("Saturday");
-                    break;
-                default: System.out.println("incorrect input");
+                sum += array[i];
             }
+        }
+        System.out.println(sum);
+    }
+    public static void indicesStandart(int[] array)
+    {
+        int number = checkValue();
+        System.out.println(Arrays.binarySearch(array, number));
+    }
+    public static void sortCycles(int[] array)
+    {
+        for(int i = 0; i < array.length; i++)
+        {
+            for(int j = i+1; j < array.length; j++)
+            {
+                if(array[j] > array[i])
+                {
+                    int count = array[i];
+                    array[i] = array[j];
+                    array[j] = count;
+                }
+            }
+        }
+        System.out.println("Sort descending");
+        System.out.println(Arrays.toString(array));
+        System.out.println("--------");
+        for(int i = 0; i < array.length; i++)
+        {
+            for(int j = i+1; j < array.length; j++)
+            {
+                if(array[j] < array[i])
+                {
+                    int count = array[i];
+                    array[i] = array[j];
+                    array[j] = count;
+                }
+            }
+        }
+        System.out.println("Sort ascending");
+        System.out.println(Arrays.toString(array));
+    }
+    public static void sortStandart(int[] array)
+    {
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+    public static void numberOfPariedCycles(int[] array)
+    {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++)
+        {
+            if(array[i] % 2 == 0 )
+            {
+                sum += 1;
+            }
+        }
+        System.out.println(sum);
+    }
+    public static void numberOfNonPariedCycles(int[] array)
+    {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++)
+        {
+            if(array[i] % 2 != 0 )
+            {
+                sum += 1;
+            }
+        }
+        System.out.println(sum);
+    }
+    public static void numberOfMultiplicityCycles(int[] array)
+    {
+        int sum = 0;
+        int number = checkValue();
+        for(int i = 0; i < array.length; i++)
+        {
+            if((array[i] % number == 0) && (array[i] > number))
+            {
+                sum += 1;
+            }
+        }
+        System.out.println(sum);
+    }
+    public static void sumEverySecondCycles(int[] array)
+    {
+        {
+            int sum = 0;
+            for(int i = 0; i < array.length; i++)
+            {
+                if((i % 2 != 0) && (i != 0))
+                {
+                    sum += array[i];
+                }
+            }
+            System.out.println(sum);
         }
     }
 
